154f5cf15972e9c8e065ea45f441a98bb0fc95b5
Tom Laplace <tomlaplace0902@gmail.com>
2024-03-07 13:53:51 +0100

    assets & monster spawn

--- Assets/Scripts/BiomeController.cs
+++ Assets/Scripts/BiomeController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class BiomeController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] monsters;
+    public GameObject[] boss;
+    public int biomeLevel = 0;
 
-    // Update is called once per frame
-    void Update()
+    public void LoadResourcesFromBiomeLevel(int biomeLevel)
     {
-        
+        monsters = null;
+        boss = null;
+        switch (biomeLevel)
+        {
+            case 0:
+                monsters = Resources.LoadAll<GameObject>("Prefabs/GothicLand/Monsters");
+                boss = Resources.LoadAll<GameObject>("Prefabs/GothicLand/Boss");
+                Debug.Log(monsters.Length);
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ Assets/Scripts/MobSpawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpawner : MonoBehaviour
+{
+    public GameObject spawnedMonster;
+
+    void Start()
+    {
+        spawnedMonster = GameObject.Find("Monster");
+    }
+
+    public void SpawnMonster(GameObject monster)
+    {
+        if (spawnedMonster != null)
+        {
+            Destroy(spawnedMonster);
+        }
+
+        spawnedMonster = Instantiate(monster, new Vector3(6, -1, 0), Quaternion.identity);
+    }
+}
